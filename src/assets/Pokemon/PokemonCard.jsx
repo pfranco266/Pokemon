637dffa690834f78bc0baa7537e68f938603d4a7
Phillip Franco <pfranco266@gmail.com>
2024-05-30 11:43:42 +0700

    fix: moved reducer into it's own file.

--- src/assets/Pokemon/PokemonCard.jsx
+++ src/assets/Pokemon/PokemonCard.jsx
@@ -2,85 +2,14 @@ import React, { useState, useEffect, useRef, useMemo, useReducer } from "react";
 import { ResistanceDescription, WeaknessDescription, GenerationDescription, IndividualPokeContainer, PokeType, PokeHeightWeight, GenerationContainer, WeaknessContainer, ElementContainer, ResistanceContainer, PokemonMoveContainer, PokemonDescriptionContainer, PokemonDescription, InfoContainer, HitPoints, Name, SpriteContainer, Sprite } from "./Pokemon.styled";
 import PreviousPokemon from "./PreviousPokemon";
 import PokemonWeak from "./PokemonWeak"
-import { GiNeedleJaws } from "react-icons/gi";
+import { pokemonReducer, initialPokeDetails } from "../Reducers/pokemonReducer";
 
 function PokemonCard({ index }) {
     const someRef = useRef(0);
 
-    const [pokemonDetails, dispatch] = useReducer(pokemonReducer, {
-        id: null,
-        legendary: false,
-        mythical: false,
-        name: '',
-        height: null,
-        weight: null,
-        sprites: {
-            default: '',
-            backDefault: '',
-            frontShiny: '',
-            backShiny: ''
-        },
-        stats: {
-            hp: null,
-            attack: null,
-            defense: null,
-            specialAttack: null,
-            specialDefense: null,
-            speed: null
-        },
-        species: {},
-        types: [],
-        moves: [],
-        evolutionTree: {},
-        description: [],
-        error: null
+    const [pokemonDetails, dispatch] = useReducer(pokemonReducer, initialPokeDetails);
 
-    });
-
-    function pokemonReducer(state, action) {
-        switch (action.type) {
-            case 'setPokemonDetails':
-                const { pokemonDetailData, pokemonSpeciesData, evolutionData } = action.payload;
-                return {
-                    ...state,
-                    id: pokemonDetailData.id,
-                    legendary: pokemonSpeciesData.is_legendary,
-                    mythical: pokemonSpeciesData.is_mythical,
-                    name: pokemonDetailData.name,
-                    height: pokemonDetailData.height / 10,
-                    weight: pokemonDetailData.weight,
-                    sprites: {
-                        default: pokemonDetailData.sprites.front_default,
-                        backDefault: pokemonDetailData.sprites.back_default,
-                        frontShiny: pokemonDetailData.sprites.front_shiny,
-                        backShiny: pokemonDetailData.sprites.back_shiny,
-                    },
-                    stats: {
-                        hp: pokemonDetailData.stats[0].base_stat,
-                        attack: pokemonDetailData.stats[1].base_stat,
-                        defense: pokemonDetailData.stats[2].base_stat,
-                        specialAttack: pokemonDetailData.stats[3].base_stat,
-                        specialDefense: pokemonDetailData.stats[4].base_stat,
-                        speed: pokemonDetailData.stats[5].base_stat,
-                    },
-                    species: evolutionData,
-                    types: pokemonDetailData.types,
-                    moves: pokemonDetailData.moves,
-                    evolutionTree: evolutionData,
-                    description: pokemonSpeciesData.flavor_text_entries.slice(0, 5).map(entry => entry.flavor_text)
-                };
-            case 'setError':
-                return {
-                    ...state,
-                    error: action.payload 
-                };
-            default:
-                return state;
-        }
-    };
-
-
-
+  
     //USE STATE SOLUTION
     // const [pokemonDetails, setPokemonDetails] = useState({
     //     id: null,
---
+++ src/assets/Reducers/pokemonReducer.js
@@ -0,0 +1,74 @@
+
+const initialPokeDetails = {
+    id: null,
+    legendary: false,
+    mythical: false,
+    name: '',
+    height: null,
+    weight: null,
+    sprites: {
+        default: '',
+        backDefault: '',
+        frontShiny: '',
+        backShiny: ''
+    },
+    stats: {
+        hp: null,
+        attack: null,
+        defense: null,
+        specialAttack: null,
+        specialDefense: null,
+        speed: null
+    },
+    species: {},
+    types: [],
+    moves: [],
+    evolutionTree: {},
+    description: [],
+    error: null
+
+};
+
+function pokemonReducer(state, action) {
+    switch (action.type) {
+        case 'setPokemonDetails':
+            const { pokemonDetailData, pokemonSpeciesData, evolutionData } = action.payload;
+            return {
+                ...state,
+                id: pokemonDetailData.id,
+                legendary: pokemonSpeciesData.is_legendary,
+                mythical: pokemonSpeciesData.is_mythical,
+                name: pokemonDetailData.name,
+                height: pokemonDetailData.height / 10,
+                weight: pokemonDetailData.weight,
+                sprites: {
+                    default: pokemonDetailData.sprites.front_default,
+                    backDefault: pokemonDetailData.sprites.back_default,
+                    frontShiny: pokemonDetailData.sprites.front_shiny,
+                    backShiny: pokemonDetailData.sprites.back_shiny,
+                },
+                stats: {
+                    hp: pokemonDetailData.stats[0].base_stat,
+                    attack: pokemonDetailData.stats[1].base_stat,
+                    defense: pokemonDetailData.stats[2].base_stat,
+                    specialAttack: pokemonDetailData.stats[3].base_stat,
+                    specialDefense: pokemonDetailData.stats[4].base_stat,
+                    speed: pokemonDetailData.stats[5].base_stat,
+                },
+                species: evolutionData,
+                types: pokemonDetailData.types,
+                moves: pokemonDetailData.moves,
+                evolutionTree: evolutionData,
+                description: pokemonSpeciesData.flavor_text_entries.slice(0, 5).map(entry => entry.flavor_text)
+            };
+        case 'setError':
+            return {
+                ...state,
+                error: action.payload 
+            };
+        default:
+            return state;
+    }
+};
+
+export {initialPokeDetails, pokemonReducer};
