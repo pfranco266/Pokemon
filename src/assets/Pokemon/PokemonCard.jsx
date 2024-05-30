55e9e390ad2dd6fca01161e5e5144b2950c44947
Phillip Franco <pfranco266@gmail.com>
2024-05-30 14:53:47 +0700

    fix: migrated fetch function to pokeAPI.js file, so i could resuse fetch logic

--- src/assets/Pokemon/PokemonCard.jsx
+++ src/assets/Pokemon/PokemonCard.jsx
@@ -3,6 +3,7 @@ import { ResistanceDescription, WeaknessDescription, GenerationDescription, Indi
 import PreviousPokemon from "./PreviousPokemon";
 import PokemonWeak from "./PokemonWeak"
 import { pokemonReducer, initialPokeDetails } from "../Reducers/pokemonReducer";
+import { fetchSinglePokemon, fetchEvolutionData } from "../Reducers/pokeAPI";
 
 function PokemonCard({ index }) {
 
@@ -12,54 +13,33 @@ function PokemonCard({ index }) {
     const [randomDescription, setRandomDescription] = useState(Math.floor(Math.random() * (pokemonDetails.description.length || 1)));
 
 
-    async function fetchSinglePokemon() {
-        try {
-            const pokemonDetailUrl = `https://pokeapi.co/api/v2/pokemon/${index}/`;
-            const pokemonSpeciesUrl = `https://pokeapi.co/api/v2/pokemon-species/${index}/`;
-
-            const [pokemonDetailData, pokemonSpeciesData] = await Promise.all([
-                fetch(pokemonDetailUrl).then(response => response.json()),
-                fetch(pokemonSpeciesUrl).then(response => response.json())
-            ]);
-
-
-            if (pokemonSpeciesData.evolution_chain?.url) {
-
-                const evolvesFromUrl = pokemonSpeciesData.evolves_from_species?.url || '';
-                const evolutionData = await fetchEvolutionData(pokemonSpeciesData.evolution_chain.url, evolvesFromUrl);
-                // console.log('evo', evolutionData, 'deailtData', pokemonDetailData, '  species', pokemonSpeciesData)
-                dispatch({
-                    type: 'setPokemonDetails',
-                    payload: { pokemonDetailData, pokemonSpeciesData, evolutionData }
-                })
-            } 
-        } catch (error) {
-            console.log(error.message)
-            dispatch({
-                type: 'error',
-                action: error.message
-            })
-
-        }
-    }
-
-    async function fetchEvolutionData(evolutionChainUrl, evolvesFromUrl) {
-        try {
-            const evolutionChainPromise = fetch(evolutionChainUrl).then(response => response.json());
-            const evolvesFromPromise = evolvesFromUrl ? fetch(evolvesFromUrl).then(response => response.json()) : Promise.resolve(null);
-
-            const [evolutionChain, evolvesFrom] = await Promise.all([evolutionChainPromise, evolvesFromPromise]);
-
-            return { ...evolutionChain, evolvesFrom };
-        } catch (error) {
-            console.error("Error fetching evolution data:", error.message);
-            return {};
-        }
-    }
-
-
     useEffect(() => {
-        fetchSinglePokemon();
+      const fetchData = async () => {  
+        try {
+          const {pokemonSpeciesData, pokemonDetailData} = await fetchSinglePokemon(index);
+          console.log('spec', pokemonDetailData);
+          console.log('pokmeonspeee', pokemonSpeciesData )
+          if (pokemonSpeciesData.evolution_chain?.url) {
+
+            const evolvesFromUrl = pokemonSpeciesData.evolves_from_species?.url || '';
+            const evolutionData = await fetchEvolutionData(pokemonSpeciesData.evolution_chain.url, evolvesFromUrl);
+        
+
+            dispatch({
+                type: 'setPokemonDetails',
+                payload: { pokemonDetailData, pokemonSpeciesData, evolutionData }
+            })
+        } 
+
+      } catch (error) {
+                  dispatch({
+            type: 'error',
+            action: error.message
+        })
+        }        
+       
+        }
+        fetchData();
     }, [index])
 
     const memoizedPokemonDetails = useMemo(() => pokemonDetails, [pokemonDetails]);
---
+++ src/assets/Reducers/pokeAPI.js
@@ -0,0 +1,51 @@
+
+
+
+async function fetchSinglePokemon(index) {
+    try {
+        const pokemonDetailUrl = `https://pokeapi.co/api/v2/pokemon/${index}/`;
+        const pokemonSpeciesUrl = `https://pokeapi.co/api/v2/pokemon-species/${index}/`;
+
+        const [pokemonDetailData, pokemonSpeciesData] = await Promise.all([
+            fetch(pokemonDetailUrl).then(response => response.json()),
+            fetch(pokemonSpeciesUrl).then(response => response.json())
+        ]);
+            console.log(pokemonSpeciesData,pokemonDetailData)
+        return {pokemonSpeciesData, pokemonDetailData};
+
+        // if (pokemonSpeciesData.evolution_chain?.url) {
+
+        //     const evolvesFromUrl = pokemonSpeciesData.evolves_from_species?.url || '';
+        //     const evolutionData = await fetchEvolutionData(pokemonSpeciesData.evolution_chain.url, evolvesFromUrl);
+        //     // console.log('evo', evolutionData, 'deailtData', pokemonDetailData, '  species', pokemonSpeciesData)
+        //     // dispatch({
+        //     //     type: 'setPokemonDetails',
+        //     //     payload: { pokemonDetailData, pokemonSpeciesData, evolutionData }
+        //     // })
+        // } 
+    } catch (error) {
+        console.log(error.message)
+//         // dispatch({
+//         //     type: 'error',
+//         //     action: error.message
+//         // })
+
+    }
+}
+
+async function fetchEvolutionData(evolutionChainUrl, evolvesFromUrl) {
+    try {
+        const evolutionChainPromise = fetch(evolutionChainUrl).then(response => response.json());
+        const evolvesFromPromise = evolvesFromUrl ? fetch(evolvesFromUrl).then(response => response.json()) : Promise.resolve(null);
+
+        const [evolutionChain, evolvesFrom] = await Promise.all([evolutionChainPromise, evolvesFromPromise]);
+      
+
+        return { ...evolutionChain, evolvesFrom };
+    } catch (error) {
+        console.error("Error fetching evolution data:", error.message);
+        return {};
+    }
+}
+
+export {fetchSinglePokemon, fetchEvolutionData};
