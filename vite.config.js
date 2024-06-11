a04acbab88f660dc61f1a60912097ed8282f3c7c
Phillip Franco <pfranco266@gmail.com>
2024-06-11 14:04:54 +0700

    fix: landing on /pokemon instead of home message

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/Pokemon/', // Ensure this matches your GitHub Pages URL path
+  base: '/Pokemon/', // bring em straight to the pokemon
   plugins: [react()],
 });
 
