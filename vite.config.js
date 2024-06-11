e5dddd2d2d1bf080ec0d54b01b0ca383de72b144
Phillip Franco <pfranco266@gmail.com>
2024-06-11 13:52:28 +0700

    fix: adjusting base URL

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,12 @@
+// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/pokemon/', 
+  base: '/Pokemon/', // Ensure this matches your GitHub Pages URL path
   plugins: [react()],
 });
+
 
 
 
