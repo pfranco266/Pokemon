5ba4c384a37b3457c9c4a535029ec6a9bba7a466
Phillip Franco <pfranco266@gmail.com>
2024-06-11 19:17:35 +0700

    change to config file routing

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/', // bring em straight to the pokemon
+  base: '/Pokemon', // bring em straight to the pokemon
   plugins: [react()],
 });
 
