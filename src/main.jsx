8cc067be09e3356b4697a4ad84a227a4cd4b05b3
Phillip Franco <pfranco266@gmail.com>
2024-06-13 12:05:59 +0700

    fix: changed config file basename, added basename formatting to browswer router, and added icon for pokeball in index.html

--- src/main.jsx
+++ src/main.jsx
@@ -10,7 +10,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
 
 
     <React.StrictMode>
-        <BrowserRouter basename="/pokemon">
+        <BrowserRouter basename={"/pokemon"}>
             <App />
         </BrowserRouter>
     </React.StrictMode>,
--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,13 @@
 
+
+
 // vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/pokemon/', // bring em straight to the pokemon
   plugins: [react()],
-  base: '/pokemon/',
-  build: {
-    outDir: 'dist',
-    rollupOptions: {
-      output: {
-        entryFileNames: `assets/[name].js`,
-        chunkFileNames: `assets/[name].js`,
-        assetFileNames: `assets/[name].[ext]`
-      }
-    }
-  },
-  server: {
-    open: '/pokemon/'
-  }
 });
 
 
@@ -26,20 +15,6 @@ export default defineConfig({
 
 
 
-
-
-
-
-
-
-// // vite.config.js
-// import { defineConfig } from 'vite';
-// import react from '@vitejs/plugin-react';
-
-// export default defineConfig({
-//   base: '/pokemon/', // bring em straight to the pokemon
-//   plugins: [react()],
-// });
 
 
 
