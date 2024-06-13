bce034caa6b0592f73cf5e6029c21e9ce1a4f564
Phillip Franco <pfranco266@gmail.com>
2024-06-13 11:36:32 +0700

    fix: config rollupoptions added to fix pathing

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,24 @@
+
 // vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/pokemon/', // bring em straight to the pokemon
   plugins: [react()],
+  base: '/pokemon/',
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      output: {
+        entryFileNames: `assets/[name].js`,
+        chunkFileNames: `assets/[name].js`,
+        assetFileNames: `assets/[name].[ext]`
+      }
+    }
+  },
+  server: {
+    open: '/pokemon/'
+  }
 });
 
 
@@ -13,10 +27,24 @@ export default defineConfig({
 
 
 
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
 
-// // https://vitejs.dev/config/
+
+
+
+
+// // vite.config.js
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+
 // export default defineConfig({
+//   base: '/pokemon/', // bring em straight to the pokemon
 //   plugins: [react()],
-// })
+// });
+
+
+
+
+
+
+
+
